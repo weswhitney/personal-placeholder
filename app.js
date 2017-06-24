da3e17b1c636a5e7ea9d113717c0d2d42defb058
weswhitney <wesleywhitney@gmail.com>
2017-06-24 16:26:45 -0600

    deployment set up

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var app = express();
 
+
+app.set('port', (process.env.PORT || 3000));
+
 app.use(express.static('assets'));
 
-
-app.listen(3000, function() {
-	console.log("listening on " + 3000);
+app.listen(app.get('port'), function() {
+	console.log("listening on ", app.get('port'));
 });
